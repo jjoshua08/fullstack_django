11d0e9b0b380df933a147900436c04ae2e74c70f
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-16 20:50:44 -0300

    configurando acervo/url.py

--- acervo/urls.py
+++ acervo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('', views.inicio),
+    path(
+        'livros/',
+        views.lista_livros,
+        name='lista'
+    ),
 ]
--- acervo/views.py
+++ acervo/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
-
-def inicio(request):
-    return HttpResponse(
-        'Olá, acervo!'
+from django.shortcuts import render
+from .models import Livro
+def lista_livros(request):
+    livros = Livro.objects.all() # busca no banco
+    return render(
+        request, 'acervo/lista.html',
+        {'livros': livros} # envia ao template
     )
