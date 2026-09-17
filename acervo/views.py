6ae8f587f254b365bf657d0301b45451278299a1
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-16 21:22:47 -0300

    atualizando acervo/views.py

--- acervo/views.py
+++ acervo/views.py
@@ -1,18 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Livro
+from .forms import LivroForm
 
 def lista_livros(request):
-    livros = Livro.objects.all() # busca no banco
-    return render(
-        request, 'acervo/lista.html',
-        {'livros': livros} # envia ao template
-    )
+    livros = Livro.objects.all()
+    return render(request, 'acervo/lista.html', {'livros': livros})
 
 def novo_livro(request):
     if request.method == 'POST':
         form = LivroForm(request.POST)
         if form.is_valid():
-            form.save() # grava no banco
+            form.save()
             return redirect('lista')
     else:
         form = LivroForm()
