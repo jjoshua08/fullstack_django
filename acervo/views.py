8377e8bd5d23138c9ac95327ee1714d1f47f3d51
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-16 21:12:38 -0300

    form.html

--- acervo/views.py
+++ acervo/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Livro
+
 def lista_livros(request):
     livros = Livro.objects.all() # busca no banco
     return render(
         request, 'acervo/lista.html',
         {'livros': livros} # envia ao template
     )
+
+def novo_livro(request):
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save() # grava no banco
+            return redirect('lista')
+    else:
+        form = LivroForm()
+    return render(request, 'acervo/form.html', {'form': form})
