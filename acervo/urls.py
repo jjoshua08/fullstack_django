1a83a31b2cdbc88731325f5f9b107ddfedd6061a
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-21 15:36:00 -0300

    implementações slide 05

--- acervo/urls.py
+++ acervo/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('livros/', views.lista_livros, name='lista'),
+    path('', views.lista_livros, name='lista'),
     path('livros/novo/', views.novo_livro, name='novo_livro'),
 ]
