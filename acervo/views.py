f572a738154d1f3a978d7439582bb6343722ba1b
jjoshua08 <tratar.quitacao_0d@icloud.com>
2026-09-21 19:07:24 -0300

    Implementado tipo de acervo, categoria e pesquisa

--- acervo/views.py
+++ acervo/views.py
@@ -4,7 +4,25 @@ from .forms import LivroForm
 
 def lista_livros(request):
     livros = Livro.objects.all()
-    return render(request, 'acervo/lista.html', {'livros': livros})
+    titulo = request.GET.get('titulo', '').strip()
+    tipo_acervo = request.GET.get('tipo_acervo', '')
+    categoria = request.GET.get('categoria', '')
+
+    if titulo:
+        livros = livros.filter(titulo__icontains=titulo)
+    if tipo_acervo:
+        livros = livros.filter(tipo_acervo=tipo_acervo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+
+    return render(request, 'acervo/lista.html', {
+        'livros': livros,
+        'titulo': titulo,
+        'tipo_acervo': tipo_acervo,
+        'categoria': categoria,
+        'tipos_acervo': Livro.TIPOS_ACERVO,
+        'categorias': Livro.CATEGORIAS,
+    })
 
 def novo_livro(request):
     if request.method == 'POST':
